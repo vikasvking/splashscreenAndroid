9e056c180ebc0f81810d9b4b2358df5add2c128c
vikasvking <vikas.vk711@gmail.com>
2017-05-04 10:51:47 +0530

    added command

--- app/src/main/java/com/haplohealth/splashscreen/MainActivity.java
+++ app/src/main/java/com/haplohealth/splashscreen/MainActivity.java
@@ -1,13 +1,23 @@
 package com.haplohealth.splashscreen;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int TIME_SPLASH=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntend=new Intent(MainActivity.this,Home.class);
+                startActivity(homeIntend);
+                finish();
+            }
+        },TIME_SPLASH);
     }
 }
